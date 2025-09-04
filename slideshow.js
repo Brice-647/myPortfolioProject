2b5e5f622c942011c80a38e1c8bb12da1589625a
Brice-647 <bputhoff@ivytech.edu>
2025-09-03 20:28:17 -0400

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+showSlides();
+
+// Manual controls
+function plusSlides(n) {
+  slideIndex += n - 1; // adjust because showSlides() increments
+  showSlides();
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+
+  slides[slideIndex-1]
+
+  // Change image every 2 seconds
+  // setTimeout(showSlides, 2000);
+}
